828949434b33ea4a55821b819038fba8fee4e82b
J-Dog1 <44008391+J-Dog1@users.noreply.github.com>
2024-12-08 14:00:42 -0800

    Create Project

---
+++ src/main/java/com/example/minecraft_stats_backend/MinecraftStatsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.minecraft_stats_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftStatsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinecraftStatsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minecraft_stats_backend/MinecraftStatsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minecraft_stats_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftStatsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
